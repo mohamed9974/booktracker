0048402073431d842dfb74c09ee19e17b5ddad53
mohamed9974 <mohamed.amin@metu.edu.tr>
2022-05-21 20:19:41 +0300

    books page backend

---
+++ functions/books_Page_backend.js
@@ -0,0 +1,30 @@
+exports = async function(payload, response) {
+
+    const {booksPerPage = 20, page = 0} = payload.query;
+  
+    let query = {};
+    // if (payload.query.isFiction) {
+    //   query = { $text: { $search: payload.query.isFiction } }
+    // } else if (payload.query.zipcode) {
+    //   query = { "address.zipcode": { $eq: payload.query.zipcode } }
+    if (payload.query.name) {
+      query = { $text: { $search: payload.query.name } }
+    }
+      
+    const collection = context.services.get("mongodb-atlas").db("booklib").collection("books");
+    let booksList = await collection.find(query).skip(page*booksPerPage).limit(booksPerPage).toArray()
+  
+    booksList.forEach(book => {
+      book._id = book._id.toString();
+    });
+  
+    const responseData = {
+      books: booksList,
+      page: page.toString(),
+      filters: {},
+      entries_per_page: booksPerPage.toString(),
+      total_results: booksList.length.toString(),
+    };
+    
+    return responseData;
+  };
