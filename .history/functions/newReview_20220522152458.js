9dfbdefa7aaa941a695dd9972d457ec1164cd617
mohamed9974 <mohamed.amin@metu.edu.tr>
2022-05-22 15:56:12 +0300

    minor changes

---
+++ .history/functions/newReview_20220522152458.js
@@ -0,0 +1,21 @@
+exports = async function(payload, response) {
+
+    if (payload.body) {
+        const body =  EJSON.parse(payload.body.text());
+        const reviews = context.services.get("mongodb-atlas").db("booklib").collection("reviews");
+        
+        const reviewDoc = {
+            title: body.name,
+            reviewer: body.user_id,
+            review: body.text,
+            book_id: BSON.ObjectId(body.book_id),
+            rating : body.rating,
+            book_title: body.book_title
+
+        };
+    
+        return await reviews.insertOne(reviewDoc);
+    }
+  
+    return  {};
+  };
