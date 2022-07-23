86e9deb4faa54f108a2504d421ef48b5e13615f5
mohamed9974 <73635450+mohamed9974@users.noreply.github.com>
2022-07-23 11:03:08 +0000

    cleared history

--- .history/frontend/src/http-common_20220523120521.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://eu-central-1.aws.realm.mongodb.com/api/client/v2.0/app/booktracker-nmprl/graphql",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
--- .history/frontend/src/models/index_20220523181304.js
+++
@@ -1,7 +0,0 @@
-
-import {createRealmContext} from '@realm/react';
-import {BooksSchema,Books_reviewsSchema,readerSchema,reader_favorite_BooksSchema,reader_reviewsSchema,reviewSchema} from './bookstoreOM';
-
-export const TaskRealmContext = createRealmContext({
-  schema: [BooksSchema,Books_reviewsSchema,readerSchema,reader_favorite_BooksSchema,reader_reviewsSchema,reviewSchema],
-});
